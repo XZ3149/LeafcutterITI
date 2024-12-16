2ed549783b9ba6e6743ba7baab345784d5332b18
Xingpei Zhang <114506836+XZ3149@users.noreply.github.com>
2024-12-16 17:38:09 -0500

    Update tealeaf_clustering.py

--- tealeaf/clustering/tealeaf_clustering.py
+++ tealeaf/clustering/tealeaf_clustering.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import sys
 import warnings
-import leafcutterITI.utils
-from leafcutterITI.utils import timing_decorator,write_options_to_file
+import tealeaf.utils
+from tealeaf.utils import timing_decorator,write_options_to_file
 from optparse import OptionParser
 warnings.simplefilter(action='ignore', category=pd.errors.DtypeWarning)
 
